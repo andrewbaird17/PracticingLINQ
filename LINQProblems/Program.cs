66b48be1672ddc7417db2f72ce8129cce186503d
Andrew Baird <baird.andrew4@gmail.com>
2020-02-07 10:32:11 -0600

    Problem 2 of removing Duplicate values from a list and return a new list without the duplicates completed

--- LINQProblems/Program.cs
+++ LINQProblems/Program.cs
@@ -12,20 +12,31 @@ namespace LINQProblems
         {
             // Problem 1
             List<string> words = new List<string> { "the", "bike", "this", "it", "tenth", "mathematics" };
-            List<string> output = LookForTH(words);
-            foreach (var item in output)
+            List<string> output1 = LookForTH(words);
+            foreach (var item in output1)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Problem 2
+            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+            List<string> output2 = RemoveDuplicate(names);
+            foreach (var item in output2)
             {
                 Console.WriteLine(item);
             }
             Console.ReadLine();
-
-
 
         }
 
         static List<string> LookForTH(List<string> input)
         {
             return input.Where(x => x.Contains("th")).ToList();
+        }
+
+        static List<string> RemoveDuplicate(List<string> input)
+        {
+            return input.Distinct().ToList();
         }
     }
 }
