95d5da494a8c9a35d2490e281cf122b2b00e85ef
Andrew Baird <baird.andrew4@gmail.com>
2020-02-07 10:27:27 -0600

    Problem 1 of finding 'th' of each substring in a list completed

--- LINQProblems/Program.cs
+++ LINQProblems/Program.cs
@@ -10,6 +10,22 @@ namespace LINQProblems
     {
         static void Main(string[] args)
         {
+            // Problem 1
+            List<string> words = new List<string> { "the", "bike", "this", "it", "tenth", "mathematics" };
+            List<string> output = LookForTH(words);
+            foreach (var item in output)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+
+
+
+        }
+
+        static List<string> LookForTH(List<string> input)
+        {
+            return input.Where(x => x.Contains("th")).ToList();
         }
     }
 }
